6d6574624825ce77ba4668e02b366c2134e489ea
JasonHongOO <83178981+JasonHongOO@users.noreply.github.com>
2021-06-16 20:41:40 +0800

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+int main(int argc,char **argv) {
+
+    char textfile[1024];
+    char line[4096];
+
+    FILE *fp;
+    if(argc > 1) {
+        strcpy(textfile,argv[1]);
+    }
+    else {
+        printf("error options\n");
+        exit(1);
+    }
+
+
+    fp = fopen(textfile, "r");
+    if(fp == NULL) {
+        fprintf(stderr, "fopen error %s\n",textfile);
+        exit(1);
+    }
+
+    char *str[4096];
+    int idx=0;
+    int check = 0;
+    while(fgets(line,4096,fp) != NULL) {
+        int len = strlen(line);
+
+        if(line[len-1] == '\n') {line[len-1] = '\0';len--;}
+        check = 1;
+
+        char temp[4096] = {0};
+        int n=0;
+        for(int i=len-1;i>=0;i--) temp[n++] = line[i];
+
+        int strlong = strlen(temp);
+        str[idx] = malloc(sizeof(char) * strlong);
+        strcpy(str[idx++],temp);
+    }
+    fclose(fp);
+
+    fp = fopen(textfile, "w");
+    for(int i=idx-1;i>=0;i--) {
+        fputs(str[i],fp);
+    }
+    fclose(fp);
+
+    for(int i=idx-1;i>=0;i--) free(str[i]);
+    return 0;
+}
